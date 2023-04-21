c605b81f17d954283abb6f9a1827d2f4b1a2ece7
aSimpleUserOrSomeone <maqubmaz@gmail.com>
2023-04-21 14:56:38 +0200

    Dialog functionality

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const path = require('path')
-const fs = require('fs')
+const express = require("express");
+const hbs = require("express-handlebars");
+const path = require("path");
+const fs = require("fs");
 
-const app = express()
-const port = 5500
+const app = express();
+const port = 5500;
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'hbs')
-app.engine('hbs', hbs.engine({ defaultLayout: 'main.hbs' }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine("hbs", hbs.engine({ defaultLayout: "main.hbs" }));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static('static'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("static"));
 
 app.get("/", (req, res) => {
-    res.render('index.hbs')
-})
+  res.render("index.hbs");
+});
 
-app.post("/newFolder", (req, res) => {
-    console.log("On post!");
-    res.render('index.hbs')
+app.post("/", (req, res) => {
+  console.log(req.body);
+  if (req.body.type === "folder") {
+  }
 
-})
-app.post("/newFile", (req, res) => {
-    res.render('index.hbs')
-
-})
+  res.render("index.hbs");
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on port: ${port}`)
-})
+  console.log(`Server listening on port: ${port}`);
+});
