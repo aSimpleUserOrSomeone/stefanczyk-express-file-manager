d8509eece0b198b893d5a66f19aeec7c4a7c4e8e
aSimpleUserOrSomeone <maqubmaz@gmail.com>
2023-04-21 15:18:52 +0200

    Server work

--- server.js
+++ server.js
@@ -13,13 +13,33 @@ app.engine("hbs", hbs.engine({ defaultLayout: "main.hbs" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("static"));
 
+const uploadsPath = path.join(__dirname, "uploads");
+if (!fs.existsSync(path.join(__dirname, "uploads"))) {
+  fs.mkdirSync(uploadsPath);
+}
+
 app.get("/", (req, res) => {
   res.render("index.hbs");
 });
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  if (req.body.type === "folder") {
+  if (req.body.reqType === "folder") {
+    console.log("is folder");
+    var newPath = path.join(uploadsPath, req.body.entityName);
+    var i = 0;
+    while (fs.existsSync(newPath)) {
+      i += 1;
+      if (newPath.at(-3) == "(" && newPath.at(-1) == ")") {
+        if (newPath.substring(newPath.length, -3) !== `(${i})`)
+          newPath += `(${i})`;
+        break;
+      } else {
+        newPath += `(${i})`;
+        break;
+      }
+    }
+    fs.mkdirSync(newPath);
   }
 
   res.render("index.hbs");
